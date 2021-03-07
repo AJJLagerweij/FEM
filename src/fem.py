Fix calculation of stiffness and transport matricces.

src/fem.py
@@ -28,6 +28,21 @@ def element_mass(phi_xq, wq_detJ):
     r"""
     Compute the elmement mass matrix.
 
+    This matrix is defined as:
+
+    .. math::
+        M = \int_{\Omega} \phi_j(x) \phi_i(x) dV
+
+    when integrated in reference element coordinates, :math:`\xi` this is:
+
+    .. math::
+        M = \int_0^1 \phi_j(\xi) \phi_i(\xi) det(J)dV
+
+    To evaluate these integras Gaussian quadrature is used such that thi integal becomes:
+
+    .. math::
+        M = \sum_{q=0}^{N_q} \phi_j(\xi_q) \phi_i(\xi_q) det(J) w_q
+
     Parameters
     ----------
     phi_xq : array_like(float), shape((dofe, num_q))
@@ -59,16 +74,31 @@ def element_mass(phi_xq, wq_detJ):
 
 
 @nb.jit(nopython=True)
-def element_transport(phi_xq, dphi_xq, wq_detJ):
+def element_transport(phi_xq, invJ_dphi_xq, wq_detJ):
     r"""
-    Compute the elmement mass matrix.
+    Compute the elmement transport matrix.
+
+    This matrix is defined as:
+
+    .. math::
+        T = \int_{\Omega} \partial_x\phi_j(x) \phi_i(x) dV
+
+    when integrated in reference element coordinates, :math:`\xi` this is:
+
+    .. math::
+        T = \int_0^1 J^{-1}\partial_{\xi}\phi_j(\xi) \phi_i(\xi) det(J)dV
+
+    To evaluate these integras Gaussian quadrature is used such that thi integal becomes:
+
+    .. math::
+        T = \sum_{q=0}^{N_q} J^{-1}\partial_{\xi}\phi_j(\xi_q) \phi_i(\xi_q) det(J) w_q
 
     Parameters
     ----------
     phi_xq : array_like(float), shape((dofe, num_q))
         For each shape function the value at the quadrature points.
-    dphi_xq : array_like(float), shape((dofe, num_q))
-        For each shape function the derivative at the quadrature points.
+    invJ_dphi_xq : array_like(float), shape((dofs, num_q))
+        For each shape function its derivative value at the quadrature points times the inverse Jacobian.
     wq_detJ : array_like(float), shape((dofe, num_q))
         Integration weight including the mapping from local to global coordinates.
 
@@ -86,19 +116,34 @@ def element_transport(phi_xq, dphi_xq, wq_detJ):
     for i in range(dofe):
         # Loop over all degrees of freedom and get matrix quantities.
         for j in range(dofe):
-            te[i, j] = np.sum(phi_xq[j] * dphi_xq[i] * wq_detJ)
+            te[i, j] = np.sum(invJ_dphi_xq[j] * phi_xq[i] * wq_detJ)
     return te
 
 
 @nb.jit(nopython=True)
-def element_stiffness(dphi_xq, wq_detJ):
+def element_stiffness(invJ_dphi_xq, wq_detJ):
     r"""
-    Compute the elmement mass matrix.
+    Compute the elmement stiffness matrix.
+
+    This matrix is defined as:
+
+    .. math::
+        S = \int_{\Omega} \partial_x\phi_j(x) \partial_x\phi_i(x) dV
+
+    when integrated in reference element coordinates, :math:`\xi` this is:
+
+    .. math::
+        S = \int_0^1 J^{-1}\partial_{\xi} \phi_j(\xi) J^{-1}\partial_{\xi}\phi_i(\xi) det(J)dV
+
+    To evaluate these integras Gaussian quadrature is used such that thi integal becomes:
+
+    .. math::
+        S = \sum_{q=0}^{N_q} J^{-1}\partial_{\xi} \phi_j(\xi_q) J^{-1}\partial_{\xi}\phi_i(\xi_q) det(J) w_q
 
     Parameters
     ----------
-    dphi_xq : array_like(float), shape((dofe, num_q))
-        For each shape function the derivative at the quadrature points.
+    invJ_dphi_xq : array_like(float), shape((dofs, num_q))
+        For each shape function its derivative value at the quadrature points times the inverse Jacobian.
     wq_detJ : array_like(float), shape((dofe, num_q))
         Integration weight including the mapping from local to global coordinates.
 
@@ -108,18 +153,18 @@ def element_stiffness(dphi_xq, wq_detJ):
         Element mass matrix.
     """
     # Create empty storage for element properties.
-    dofe = len(dphi_xq)
+    dofe = len(invJ_dphi_xq)
     se = np.zeros((dofe, dofe))
 
     # Quadrature summations handeled by numpy.sum().
     # Loop over all degrees of freedom and get:
     for i in range(dofe):
         # Matrix diagonal quantiy.
-        se[i, i] = np.sum(dphi_xq[i] * dphi_xq[i] * wq_detJ)
+        se[i, i] = np.sum(invJ_dphi_xq[i] * invJ_dphi_xq[i] * wq_detJ)
 
         # Loop over all degrees of freedom and get matrix quantities.
         for j in range(i+1, dofe):
-            me_ij = np.sum(dphi_xq[j] * dphi_xq[i] * wq_detJ)
+            me_ij = np.sum(invJ_dphi_xq[j] * invJ_dphi_xq[i] * wq_detJ)
             se[i, j] = me_ij
             se[j, i] = me_ij
     return se
@@ -230,7 +275,7 @@ def kernel1d(x, c, rhs, num_q, order, mass=False, transport=False, stiffness=Fal
         # Obtain element properties
         dofe = c[ele]
         x_ele = x[dofe]
-        phi_xq, dphi_xq, f_xq, wq_detJ = get_element(num_q, x_ele, rhs, order=order)
+        phi_xq, invJ_dphi_xq, f_xq, wq_detJ = get_element(num_q, x_ele, rhs, order=order)
 
         # Perform integration and compute right hand side vector.
         if rhs != None:
@@ -247,7 +292,7 @@ def kernel1d(x, c, rhs, num_q, order, mass=False, transport=False, stiffness=Fal
             m_j[ele] = je
 
         if transport == True:
-            me = element_transport(phi_xq, dphi_xq, wq_detJ)
+            me = element_transport(phi_xq, invJ_dphi_xq, wq_detJ)
             ie = np.repeat(dofe, len(dofe))
             je = ie.reshape((-1, len(dofe))).T.ravel()
             m_v[ele] = me.ravel()
@@ -255,7 +300,7 @@ def kernel1d(x, c, rhs, num_q, order, mass=False, transport=False, stiffness=Fal
             m_j[ele] = je
 
         if stiffness == True:
-            me = element_stiffness(dphi_xq, wq_detJ)
+            me = element_stiffness(invJ_dphi_xq, wq_detJ)
             ie = np.repeat(dofe, len(dofe))
             je = ie.reshape((-1, len(dofe))).T.ravel()
             m_v[ele] = me.ravel()

src/pde.py
@@ -36,46 +36,46 @@ import scipy.sparse as sparse
 from fem import kernel1d
 
 
-def projection(x, c, fun, num_q, order):
+def projection(x, connect, fun, num_q, order):
     r"""
     Projecting a 1D function :math:`f(x)` on a finite element basis.
 
     Lets create our approximation function,
 
     .. math::
-        f_h(x) = \sum_{n=0}^N u_n \phi_n(x)
+        f_h(x) = \sum_{n=0}^N \bar{u}_n \phi_n(x)
 
-    as a weighted summation of the basisfunctions of approximation.
-    Where :math:`phi_n` are the basisfunctions of our FE space :math:`V_h`.
-    The unknows here are the weights :math:`f_n`.
-    To find these weights we formulate a weak form:
+    as a weighted summation of the basisfunctions of approximation. Where :math:`phi_n` are the basisfunctions of our
+    FE space :math:`V_h`. The unknows here are the weights :math:`\bar{u}_n`, these we call degrees of freedom.
+    To find these DOFs we formulate a weak form:
 
     .. math::
         \int_\Omega (f_h(x) - f(x)) \, \phi_i(x)\, dV = 0 \quad \forall \quad \phi_i \in V_h
 
-    in which we substitute our approximation function and separate the knowns from the unknowns.
-    We find:
+    in which we substitute our approximation function and separate the knowns from the unknowns. We find:
 
     .. math::
-         \int_\Omega \phi_i(x) \sum_{j=0}^N u_j \phi_j(x)\, dV = \int_\Omega \phi_i(x) f(x) \, dV \quad \forall \quad \phi_i \in V_h
+         \int_\Omega \phi_i(x) \sum_{j=0}^N \bar{u}_j \phi_j(x)\, dV =
+         \int_\Omega \phi_i(x) f(x) \, dV \quad \forall \quad \phi_i \in V_h
 
-    As the weights :math:`u_n` are independent of location, we can take them out of the integral:
+    As the weights :math:`\bar{u}_n` are independent of location, we can take them out of the integral:
 
     .. math::
-        \sum_{j=0}^N u_j \int_\Omega \phi_i(x) \phi_j(x)\, dV = \int_\Omega \phi_i(x) f(x)\, dV \quad \forall \quad \phi_i \in V_h
+        \sum_{j=0}^N \bar{u}_j \int_\Omega \phi_i(x) \phi_j(x)\, dV =
+        \int_\Omega \phi_i(x) f(x)\, dV \quad \forall \quad \phi_i \in V_h
 
     Which can be rewritten as a system of linear equations, which is:
 
     .. math::
-        M \, u = b
+        M \, \bar{u} = b
 
-    Where :math:`M` is a matrix and :math:`u` and :math:`b` are vectors.
+    Where :math:`M` is a matrix and :math:`\bar{u}` and :math:`b` are vectors.
 
     Parameters
     ----------
     x : array_like(float)
         Global coordinates of all degrees of freedom.
-    c : array_like(int), shape((num_ele, dofe/ele))
+    connect : array_like(int), shape((num_ele, dofe/ele))
         Element to degree of freedom connectivety map.
     fun : callable
         Function that acts as our right hand side (nonhomogeneous term).
@@ -92,31 +92,75 @@ def projection(x, c, fun, num_q, order):
         The right hand side, caused by the non-homogeneous behavior.
     """
     # Obtain matrix elements from main FEM kernel.
-    M, T, S, b = kernel1d(x, c, fun, num_q, order, mass=True)
+    M, T, S, b = kernel1d(x, connect, fun, num_q, order, mass=True)
 
     # Convert matrix objects to sparse counterparts.
     M = sparse.coo_matrix(M).tocsr()
     return M, b
 
 
-def diffusive(x, c, mu, num_q, order):
+def diffusive(x, connect, mu, num_q, order):
     r"""
-    Time derivative of the PDE for advective diffusive problems.
+    Time derivative of the PDE for diffusion problems.
 
     .. math::
         u_{t} - \mu u_{xx} = 0 \qquad \forall \, x \in \Omega = [0, L] \quad \& \quad t>0
 
-    is discretized though FE such that it results in linear algabra objects.
-    This FE projection represents the following equation:
+    Which, is converted into a weak form through:
 
     .. math::
-        M u_t = K u + b
+        \int_\Omega (\tilde{u}_t - \mu \tilde{u}_{xx} ) \phi_i(x) dV = 0 \quad \forall \quad \phi_i \in V_h
+
+    Where :math:`\tilde{u}` is our approximation:
+
+    .. math::
+        \tilde{u}(x) = \sum_{n = 1}^N \bar{u}_n \phi_n(x)
+
+    in which :math:`\bar{u}_n` are the unknowns and :math:`\phi_n(x)` are the basisfunctions of
+    FE approximation space :math:`V_h`. Substituting this approximation leads to:
+
+    .. math::
+        \int_\Omega (\partial_t\sum_{j = 1}^N \bar{u}_j \phi_j(x) - \mu \partial_{xx}\sum_{j = 1}^N \bar{u}_j \phi_j(x) ) \phi_i(x)
+        dV = 0 \quad \forall \quad \phi_i \in V_h
+
+    Which we split into different integrals:
+
+    .. math::
+        \int_\Omega  (\partial_t \sum_{j = 1}^N \bar{u}_j \phi_j(x))\phi_i(x) dV -
+        \int_\Omega  (\mu\partial_{xx}\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV = 0 \quad \forall \quad \phi_i \in V_h
+
+    For the first integral we notice that the basis functions are constant through time, only the degrees of freedom
+    :math:`\bar{u}_j` vary through time. Similarly these degrees of freedom does not affect the integral over space
+    :math:`\int_\Omega dV`. Thus we can write:
+
+    .. math::
+        \int_\Omega (\partial_t \sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        \sum_{j = 1}^N \partial_t \bar{u}_j \int_\Omega \phi_j(x) \phi_i(x) dV = M \bar{u}_j
+
+    where :math:`M` is the mass matrix which combines the integral for all different basis functions.
+    For the second term we apply integration by parts, while assuming that Diriclet boundary conditions:
+
+    .. math::
+        \int_\Omega (\mu \partial_{xx}\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        \int_\Omega (\mu \partial_x\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \partial_x\phi_i(x) dV =
+        \mu \sum_{j=1}^N \bar{u}_j \int_\Omega \partial_x \phi_j(x) \partial_x\phi_i(x) dV = \mu S \bar{u}_j
+
+    where :math:`S` is the stiffness matrix, which can be computed independently from the actual unknowns.
+    Now we can write our PDE in terms of linear algabra objects:
+
+    .. math::
+        M \bar{u}_t - \mu S \bar{u} = 0
+
+    which we modify to be in the format is expected by the temporal solvers:
+
+    .. math::
+        M \bar{u}_t = K \bar{u}
 
     Parameters
     ----------
     x : array_like(float)
         Global coordinates of all degrees of freedom.
-    c : array_like(int), shape((num_ele, dofe/ele))
+    connect : array_like(int), shape((num_ele, dofe/ele))
         Element to degree of freedom connectivety map.
     mu : float
         Diffusive constant.
@@ -130,14 +174,211 @@ def diffusive(x, c, mu, num_q, order):
     M : matrix, (sparse csr format)
         The mass matrix.
     K : matrix, (sparse csr format)
-        The negative stiffeness matrix.
+        The stiffeness matrix scaled with the diffusivity constant :math:`K = \mu S`.
     b : vector, (dense array)
-        The right hand side, caused by the non-homogeneous behavior.
+        The right hand side, because we consider a homogeneous PDE with diriclet conditions it is a zero vector.
     """
     # Obtain matrix elements from main FEM kernel.
-    M, T, S, b = kernel1d(x, c, fun, num_q, order, mass=True, stiffness=True)
+    M, T, S, b = kernel1d(x, connect, None, num_q, order, mass=True, stiffness=True)
 
     # Convert matrix objects to sparse counterparts.
     M = sparse.coo_matrix(M).tocsr()
     K = mu * sparse.coo_matrix(S).tocsr()
+    return M, K, b
+
+
+def advective(x, connect, c, num_q, order):
+    r"""
+    Time derivative of the PDE for advective diffusive problems.
+
+    .. math::
+        u_{t} + c u_x = 0 \qquad \forall \, x \in \Omega = [0, L] \quad \& \quad t>0
+
+    Which, is converted into a weak form through:
+
+    .. math::
+        \int_\Omega (\tilde{u}_t + c \tilde{u}_x ) \phi_i(x) dV =
+        0 \quad \forall \quad \phi_i \in V_h
+
+    Where :math:`\tilde{u}` is our approximation:
+
+    .. math::
+        \tilde{u}(x) = \sum_{n = 1}^N \bar{u}_n \phi_n(x)
+
+    in which :math:`\bar{u}_n` are the unknowns, socalled degrees of freedom and :math:`\phi_n(x)` are the
+    basisfunctions of FE approximation space :math:`V_h`. Substituting this approximation leads to:
+
+    .. math::
+        \int_\Omega (\partial_t\sum_{j = 1}^N \bar{u}_j \phi_j(x) +
+        c \partial_x \sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV
+        = 0 \quad \forall \quad \phi_i \in V_h
+
+    Which we split into different integrals:
+
+    .. math::
+        \int_\Omega  (\partial_t \sum_{j = 1}^N \bar{u}_j \phi_j(x))\phi_i(x) dV +
+        \int_\Omega  (c\partial_x\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        0 \quad \forall \quad \phi_i \in V_h
+
+    For the first integral we notice that the basis functions are constant through time, only the degrees of freedom
+    :math:`\bar{u}_j` vary through time. Similarly these degrees of freedom does not affect the integral over space
+    :math:`\int_\Omega dV`. Thus we can write:
+
+    .. math::
+        \int_\Omega (\partial_t \sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        \sum_{j = 1}^N \partial_t \bar{u}_j \int_\Omega \phi_j(x) \phi_i(x) dV = M \bar{u}_j
+
+    where :math:`M` is the mass matrix which combines the integral for all different basis functions.
+    For the second term we aknoledge that the degrees of freedom have no spatial and temporal effects thus
+    we can take them out of the integral and derivatives.
+
+    .. math::
+        \int_\Omega  (c\partial_x\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        c \sum_{j = 1}^N \bar{u}_j \int_\Omega \partial_x\phi_j(x) \phi_i(x) dV = c T \bar{u}
+
+    where :math:`T` is the socalled transport matrix, wich only depends on the basis functions.
+
+    Now we can write our PDE in terms of linear algabra objects:
+
+    .. math::
+        M \bar{u}_t + c T \bar{u} = 0
+
+    which we modify to be in the format is expected by the temporal solvers:
+
+    .. math::
+        M \bar{u}_t = K \bar{u}
+
+    Parameters
+    ----------
+    x : array_like(float)
+        Global coordinates of all degrees of freedom.
+    connect : array_like(int), shape((num_ele, dofe/ele))
+        Element to degree of freedom connectivety map.
+    c : float
+        Advective constant.
+    num_q : int
+        Number of Gausian quadrature points.
+    order : int
+        Order of the polynomial used by our element.
+
+    Returns
+    -------
+    M : matrix, (sparse csr format)
+        The mass matrix.
+    K : matrix, (sparse csr format)
+        The combination of stiffness and transport matrix matrix scaled with the approprate constants
+         :math:`K = - c T`.
+    b : vector, (dense array)
+        The right hand side, because we consider a homogeneous PDE with diriclet conditions it is a zero vector.
+    """
+    # Obtain matrix elements from main FEM kernel.
+    M, T, S, b = kernel1d(x, connect, None, num_q, order, mass=True, transport=True)
+
+    # Convert matrix objects to sparse counterparts.
+    M = sparse.coo_matrix(M).tocsr()
+    K = - c * sparse.coo_matrix(T).tocsr()
+    return M, K, b
+
+
+def advectivediffusive(x, connect, c, mu, num_q, order):
+    r"""
+    Time derivative of the PDE for advective diffusive problems.
+
+    .. math::
+        u_{t} + c u_x - \mu u_{xx} = 0 \qquad \forall \, x \in \Omega = [0, L] \quad \& \quad t>0
+
+    Which, is converted into a weak form through:
+
+    .. math::
+        \int_\Omega (\tilde{u}_t + c \tilde{u}_x - \mu \tilde{u}_{xx} ) \phi_i(x) dV =
+        0 \quad \forall \quad \phi_i \in V_h
+
+    Where :math:`\tilde{u}` is our approximation:
+
+    .. math::
+        \tilde{u}(x) = \sum_{n = 1}^N \bar{u}_n \phi_n(x)
+
+    in which :math:`\bar{u}_n` are the unknowns, socalled degrees of freedom and :math:`\phi_n(x)` are the
+    basisfunctions of FE approximation space :math:`V_h`. Substituting this approximation leads to:
+
+    .. math::
+        \int_\Omega (\partial_t\sum_{j = 1}^N \bar{u}_j \phi_j(x) +
+        c \partial_x \sum_{j = 1}^N \bar{u}_j \phi_j(x) -
+        \mu \partial_{xx}\sum_{j = 1}^N \bar{u}_j \phi_j(x) ) \phi_i(x) dV
+        = 0 \quad \forall \quad \phi_i \in V_h
+
+    Which we split into different integrals:
+
+    .. math::
+        \int_\Omega  (\partial_t \sum_{j = 1}^N \bar{u}_j \phi_j(x))\phi_i(x) dV +
+        \int_\Omega  (c\partial_x\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV -
+        \int_\Omega  (\mu\partial_{xx}\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        0 \quad \forall \quad \phi_i \in V_h
+
+    For the first integral we notice that the basis functions are constant through time, only the degrees of freedom
+    :math:`\bar{u}_j` vary through time. Similarly these degrees of freedom does not affect the integral over space
+    :math:`\int_\Omega dV`. Thus we can write:
+
+    .. math::
+        \int_\Omega (\partial_t \sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        \sum_{j = 1}^N \partial_t \bar{u}_j \int_\Omega \phi_j(x) \phi_i(x) dV = M \bar{u}_j
+
+    where :math:`M` is the mass matrix which combines the integral for all different basis functions.
+    For the second term we aknoledge that the degrees of freedom have no spatial and temporal effects thus
+    we can take them out of the integral and derivatives.
+
+    .. math::
+        \int_\Omega  (c\partial_x\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        c \sum_{j = 1}^N \bar{u}_j \int_\Omega \partial_x\phi_j(x) \phi_i(x) dV = c T \bar{u}
+
+    where :math:`T` is the socalled transport matrix, wich only depends on the basis functions.
+    For the thrird part we apply integration by parts, while assuming that Diriclet boundary conditions:
+
+    .. math::
+        \int_\Omega (\mu \partial_{xx}\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \phi_i(x) dV =
+        \int_\Omega (\mu \partial_x\sum_{j = 1}^N \bar{u}_j \phi_j(x)) \partial_x\phi_i(x) dV =
+        \mu \sum_{j=1}^N \bar{u}_j \int_\Omega \partial_x \phi_j(x) \partial_x\phi_i(x) dV = \mu S \bar{u}_j
+
+    where :math:`S` is the stiffness matrix, which can be computed independently from the actual unknowns.
+    Now we can write our PDE in terms of linear algabra objects:
+
+    .. math::
+        M \bar{u}_t + c T \bar{u} - \mu S \bar{u} = 0
+
+    which we modify to be in the format is expected by the temporal solvers:
+
+    .. math::
+        M \bar{u}_t = K \bar{u}
+
+    Parameters
+    ----------
+    x : array_like(float)
+        Global coordinates of all degrees of freedom.
+    connect : array_like(int), shape((num_ele, dofe/ele))
+        Element to degree of freedom connectivety map.
+    c : float
+        Advective constant.
+    mu : float
+        Diffusive constant.
+    num_q : int
+        Number of Gausian quadrature points.
+    order : int
+        Order of the polynomial used by our element.
+
+    Returns
+    -------
+    M : matrix, (sparse csr format)
+        The mass matrix.
+    K : matrix, (sparse csr format)
+        The combination of stiffness and transport matrix matrix scaled with the approprate constants
+         :math:`K = \mu S - c T`.
+    b : vector, (dense array)
+        The right hand side, because we consider a homogeneous PDE with diriclet conditions it is a zero vector.
+    """
+    # Obtain matrix elements from main FEM kernel.
+    M, T, S, b = kernel1d(x, connect, None, num_q, order, mass=True, transport=True, stiffness=True)
+
+    # Convert matrix objects to sparse counterparts.
+    M = sparse.coo_matrix(M).tocsr()
+    K = mu * sparse.coo_matrix(S).tocsr() - c * sparse.coo_matrix(T).tocsr()
     return M, K, b